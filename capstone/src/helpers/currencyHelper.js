f1d4727bc47bc78c0bcf8eb671cf1be56311edf0
Stephen Doe <donsimon505@gmail.com>
2025-08-28 14:22:39 +0000

    Addition of Supported Currencies list and CurrencyHelper

---
+++ capstone/src/helpers/currencyHelper.js
@@ -0,0 +1,47 @@
+import currencies from "./currencies.json";
+
+// Fetching Currency flag Url
+
+export const getFlagUrl = (countryCode) =>
+  `https://flagcdn.com/w40/${countryCode.toLowerCase()}.png`;
+
+// Special Multi-country currencies
+export const getCurrencyFlag = (currencyCode) => {
+  switch (currencyCode) {
+    case "EUR":
+      return getFlagUrl("eu");
+
+    case "USD":
+      return getFlagUrl("us");
+
+    case "GBP":
+      return getFlagUrl("gb");
+
+    case "XAF":
+    case "XOF":
+    case "XCD":
+    case "XPF":
+      return getFlagUrl("fr"); //Francs
+
+    default:
+      return getFlagUrl(currencyCode.substring(0, 2));
+  }
+};
+
+//Getting full currency data
+export const getCurrencyData = (currencyCode) => {
+  const base = currencies[currencyCode];
+  if (!base) return null;
+
+  return {
+    code: currencyCode,
+    ...base,
+    flag: getCurrencyFlag(currencyCode),
+  };
+};
+
+// Listing all supported currencies for API
+export const listAllCurrencies = () =>
+  Object.keys(currencies).map((code) => getCurrencyData(code));
+
+export default currencies;
